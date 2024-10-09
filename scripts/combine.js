825798e25cd54eb1b0d8d166084b9d042f8bd4bf
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2024-10-09 16:33:52 -0400

    Switch from xml2js to xmlbuilder2

--- scripts/combine.js
+++ scripts/combine.js
@@ -1,8 +1,8 @@
 // Check every section for required data, combine all sections into one gpx, export distance,
 // selevation, surface analysis (for sections and entire route)
 
-const { readdir, readFile, writeFile } = require("fs/promises");
-const xml2js = require("xml2js");
+const fs = require("node:fs/promises");
+const xmlbuilder2 = require("xmlbuilder2");
 const {
   summary,
   parseSegments,
@@ -13,7 +13,7 @@ const {
 const pathToSections = __dirname + "/../sections";
 
 async function getDirectories(source) {
-  return (await readdir(source, { withFileTypes: true }))
+  return (await fs.readdir(source, { withFileTypes: true }))
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
 }
@@ -23,7 +23,7 @@ async function getSectionNames() {
 }
 
 async function verifySectionIntegrity(sectionName) {
-  const dirNames = await readdir(pathToSections + "/" + sectionName);
+  const dirNames = await fs.readdir(pathToSections + "/" + sectionName);
   return ["geojson", "org"].every((ext) =>
     dirNames.includes(`${sectionName}.${ext}`)
   );
@@ -49,7 +49,7 @@ async function run() {
   const sections = await Promise.all(
     sectionNames.map(async (sectionName) => {
       const gj = JSON.parse(
-        await readFile(
+        await fs.readFile(
           `${pathToSections}/${sectionName}/${sectionName}.geojson`
         )
       );
@@ -68,7 +68,7 @@ async function run() {
   // Write individual section analyses
   await Promise.all(
     sections.map(({ name, segments, filteredAscend }) => {
-      writeFile(
+      fs.writeFile(
         `${pathToSections}/${name}/${name}-analysis.json`,
         JSON.stringify(summary(segments, filteredAscend), null, 2)
       );
@@ -81,7 +81,7 @@ async function run() {
     (acc, section) => [...acc, ...section.segments],
     []
   );
-  await writeFile(
+  await fs.writeFile(
     `${__dirname}/../route-analysis.json`,
     JSON.stringify(
       summary(
@@ -94,28 +94,34 @@ async function run() {
   );
   console.log("Wrote route section analysis");
 
-  // Combine point and make route.gpx from skeleton
-  const parser = new xml2js.Parser();
-  const builder = new xml2js.Builder();
-  const gpxObj = await readFile(`${__dirname}/../route-skeleton.gpx`).then(
-    parser.parseStringPromise
-  );
+  // Combine points and make route.gpx
   const allPoints = sections.reduce((acc, section) => {
     return [...acc, ...section.points];
   }, []);
-  gpxObj.gpx.trk[0].trkseg = [
-    {
-      trkpt: allPoints.map((point) => ({
-        $: { lon: `${point[0]}`, lat: `${point[1]}` },
-        ele: [`${point[2]}`],
-      })),
+  const gpxObj = {
+    gpx: {
+      "@xmlns": "http://www.topografix.com/GPX/1/1",
+      "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+      "@xsi:schemaLocation":
+        "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd",
+      "@creator": "BRouter-1.6.1",
+      "@version": "1.1",
+      trk: {
+        name: `CT Nutmeg Bikepacking Route ${new Date().toISOString()}`,
+        trkseg: {
+          trkpt: allPoints.map((point) => ({
+            "@lon": `${point[0]}`,
+            "@lat": `${point[1]}`,
+            ele: `${point[2]}`,
+          })),
+        },
+      },
     },
-  ];
-  gpxObj.gpx.trk[0].name[0] = `CT Nutmeg Bikepacking Route ${
-    new Date().toISOString().split("T")[0]
-  }`;
-  const xml = builder.buildObject(gpxObj);
-  await writeFile(`${__dirname}/../route.gpx`, xml);
+  };
+  const doc = xmlbuilder2.create(gpxObj);
+  doc.dec({ version: "1.0", encoding: "UTF-8", standalone: true });
+  const xml = doc.end({ prettyPrint: true });
+  await fs.writeFile(`${__dirname}/../route.gpx`, xml);
   console.log("Combined gpx and saved route.gpx (done)");
 
   // Export surface fixmes
@@ -141,7 +147,7 @@ async function run() {
         3
       )}`,
     }));
-  await writeFile(
+  await fs.writeFile(
     `${__dirname}/../route-surface-fixmes.json`,
     JSON.stringify(surfaceFixmes, null, 2)
   );
